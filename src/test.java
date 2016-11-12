d26a46517122645a39012e2dc05482345e8e7abd
lsq4590578 <lsq_4590578@126.com>
2016-11-12 14:01:36 +0800

    change test code

--- src/test.java
+++ src/test.java
@@ -4,5 +4,9 @@
 public class test {
     public static  void main(String[] agrs){
         System.out.println("Hello github");
+        if (int =0;i<=5;i++){
+            System.out.println("welecome to github");
+        }
     }
+
 }
